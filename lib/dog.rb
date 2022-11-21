87100f9e5f67520d52522c7069f49cd4a46a3098
Bernard Legaspi <blegaspi26@gmail.com>
2022-11-21 17:16:37 -0500

    commit tree 41a946670ba21ae50fe0e8698a83298af8046c0b

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+
+# require 'pry'
+
+# sit = Dog.new
+#     puts("The Dog is sitting")
+# end 
+# end
+
+# binding.pry
+
+class Dog 
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end 
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+# require 'pry'
+
+# talk = Person.new
+#     puts("Hello World!")
+# end 
+
+# walk = Person.new
+#     puts("The Person is walking")
+# end
+
+# binding.pry
+
+class Person
+    def talk 
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end 
+end
